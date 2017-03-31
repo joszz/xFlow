dc3d54c70f81f99fe630ed781198c920a78976f7
Jos Nienhuis <jos_nienhuis@hotmail.com>
2017-04-01 00:15:07 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/** The configuration file used in this project
+ *  @package    Framework
+ *  @version    0.1
+ *  @author     Jos Nienhuis
+ *  @copyright  Copyright (c) 2013 Jos Nienhuis
+ *  @since      04-04-2014
+ */
+
+$settings['db']['host']             = 'localhost';
+$settings['db']['user']             = 'user';
+$settings['db']['pass']             = 'password';
+$settings['db']['dbname']['video']  = 'MyVideos107';
+
+$settings['debug'] = false;
+
+define('WWWBASE', '/xflow/');
+?>
